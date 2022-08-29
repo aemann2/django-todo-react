93593f4dfe969aed8eb0a3f3be28637a235bb1e1
aemann2 <aemann2@gmail.com>
2022-08-29 07:16:56 -0700

    Converted Modal to functional component

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import {useState} from "react";
 import {
   Button,
   Modal,
@@ -11,30 +11,23 @@ import {
   Label,
 } from "reactstrap";
 
-export default class CustomModal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeItem: this.props.activeItem,
-    };
-  }
+const CustomModal = (props) => {
+  const [activeItem, setActiveItem] = useState(props.activeItem);
+  const {toggle, onSave} = props;
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     let { name, value } = e.target;
 
     if (e.target.type === "checkbox") {
       value = e.target.checked;
     }
 
-    const activeItem = { ...this.state.activeItem, [name]: value };
+    const item = { ...activeItem, [name]: value };
 
-    this.setState({ activeItem });
+    setActiveItem(item);
   };
 
-  render() {
-    const { toggle, onSave } = this.props;
-
-    return (
+      return (
       <Modal isOpen={true} toggle={toggle}>
         <ModalHeader toggle={toggle}>Todo Item</ModalHeader>
         <ModalBody>
@@ -45,8 +38,8 @@ export default class CustomModal extends Component {
                 type="text"
                 id="todo-title"
                 name="title"
-                value={this.state.activeItem.title}
-                onChange={this.handleChange}
+                value={activeItem.title}
+                onChange={handleChange}
                 placeholder="Enter Todo Title"
               />
             </FormGroup>
@@ -56,8 +49,8 @@ export default class CustomModal extends Component {
                 type="text"
                 id="todo-description"
                 name="description"
-                value={this.state.activeItem.description}
-                onChange={this.handleChange}
+                value={activeItem.description}
+                onChange={handleChange}
                 placeholder="Enter Todo description"
               />
             </FormGroup>
@@ -66,8 +59,8 @@ export default class CustomModal extends Component {
                 <Input
                   type="checkbox"
                   name="completed"
-                  checked={this.state.activeItem.completed}
-                  onChange={this.handleChange}
+                  checked={activeItem.completed}
+                  onChange={handleChange}
                 />
                 Completed
               </Label>
@@ -77,12 +70,13 @@ export default class CustomModal extends Component {
         <ModalFooter>
           <Button
             color="success"
-            onClick={() => onSave(this.state.activeItem)}
+            onClick={() => onSave(activeItem)}
           >
             Save
           </Button>
         </ModalFooter>
       </Modal>
     );
-  }
 }
+
+export default CustomModal;
